bff947fb4bb32bd9696d9bdad41fcc1e965d4148
markup54 <marzenakupka@tlen.pl>
2022-10-27 08:57:31 +0200

    wypelnianie widokow kolejnymi pytaniami

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,17 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button sprawdzButton;
     Button podpowiedzButton;
     RadioGroup odpowiedziRadioGroup;
+    TextView pytanieTextView;
+    RadioButton odpaRadioButton;
+    RadioButton odpbRadioButton;
+    RadioButton odpcRadioButton;
+    ImageView imageView;
+    ArrayList<Pytanie> pytanka;
+    int aktualnyIndeks =0;//będzie zerem piszemy dla przejrzystości:)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pytanieTextView = findViewById(R.id.textViewPytanie);
+        odpaRadioButton = findViewById(R.id.radioButton);
+        odpbRadioButton = findViewById(R.id.radioButton2);
+        odpcRadioButton = findViewById(R.id.radioButton3);
+        imageView = findViewById(R.id.imageView);
+
+        Pytanie.przygotujPytania();
+        pytanka = Pytanie.pytaniaArrayList;
+        wypelnijWidokiPytaniem(aktualnyIndeks);
+
         sprawdzButton = findViewById(R.id.button);
         sprawdzButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -26,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this,
                                     R.string.ok,
                                     Toast.LENGTH_SHORT).show();
+                            aktualnyIndeks++;
+                            wypelnijWidokiPytaniem(aktualnyIndeks);
                         }
                         else{
                             Toast.makeText(MainActivity.this,
@@ -55,4 +79,17 @@ public class MainActivity extends AppCompatActivity {
         else
             return false;
     }
+
+    void wypelnijWidokiPytaniem(int i){
+        Pytanie aktualnePytanie = pytanka.get(i);
+        pytanieTextView.setText(aktualnePytanie.getTresc());
+        odpaRadioButton.setText(aktualnePytanie.getOdpowiedzi().get(0));
+        odpaRadioButton.setText(aktualnePytanie.getOdpowiedzi().get(1));
+        odpaRadioButton.setText(aktualnePytanie.getOdpowiedzi().get(2));
+        imageView.setImageResource(aktualnePytanie.getIdObrazka());
+        imageView.setContentDescription("Jakaś podpowiedź");//TODO poprawić
+
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/Pytanie.java
+++ app/src/main/java/com/example/myapplication/Pytanie.java
@@ -3,25 +3,47 @@ package com.example.myapplication;
 import java.util.ArrayList;
 
 public class Pytanie {
-    private  String tresc;
+    private  int tresc;
     private  int idObrazka;
-    private ArrayList<String> odpowiedzi;
+    private ArrayList<Integer> odpowiedzi;
     private int odpowiedz;
-    private String podpowiedz;
+    private int podpowiedz;
 
-    public Pytanie(String tresc, int idObrazka, ArrayList<String> odpowiedzi, int odpowiedz, String podpowiedz) {
-        this.tresc = tresc;
-        this.idObrazka = idObrazka;
-        this.odpowiedzi = odpowiedzi;
-        this.odpowiedz = odpowiedz;
-        this.podpowiedz = podpowiedz;
+    public static ArrayList<Pytanie> pytaniaArrayList;
+    public static void przygotujPytania(){
+        pytaniaArrayList = new ArrayList<>();
+        pytaniaArrayList.add(new Pytanie(R.string.pytanie,
+                R.drawable.tarnica, 1,R.string.opis,
+                R.string.odp_a,R.string.odp_b,R.string.odp_c));
+        pytaniaArrayList.add(new Pytanie(R.string.pytanie2,
+                R.drawable.mnich, 2,R.string.opis2,
+                R.string.odp_a2,R.string.odp_b2,R.string.odp_c2));
+        pytaniaArrayList.add(new Pytanie(R.string.pytanie3,
+                R.drawable.rysianka, 0,R.string.opis3,
+                R.string.odp_a3,R.string.odp_b3,R.string.odp_c3));
+        pytaniaArrayList.add(new Pytanie(R.string.pytanie4,
+                R.drawable.stozek, 1,R.string.opis4,
+                R.string.odp_a4,R.string.odp_b4,R.string.odp_c4));
     }
 
-    public String getTresc() {
+
+    public Pytanie(int tresc, int idObrazka, int odpowiedz,
+                   int podpowiedz, int odpa,int odpb, int odpc) {
+        this.tresc = tresc;
+        this.idObrazka = idObrazka;
+        this.odpowiedz = odpowiedz;
+        this.podpowiedz = podpowiedz;
+        this.odpowiedzi = new ArrayList<Integer>();
+        this.odpowiedzi.add(odpa);
+        this.odpowiedzi.add(odpb);
+        this.odpowiedzi.add(odpc);
+    }
+
+    public int getTresc() {
         return tresc;
     }
 
-    public void setTresc(String tresc) {
+    public void setTresc(int tresc) {
         this.tresc = tresc;
     }
 
@@ -33,11 +55,12 @@ public class Pytanie {
         this.idObrazka = idObrazka;
     }
 
-    public ArrayList<String> getOdpowiedzi() {
+    public ArrayList<Integer> getOdpowiedzi() {
+
         return odpowiedzi;
     }
 
-    public void setOdpowiedzi(ArrayList<String> odpowiedzi) {
+    public void setOdpowiedzi(ArrayList<Integer> odpowiedzi) {
         this.odpowiedzi = odpowiedzi;
     }
 
@@ -49,11 +72,11 @@ public class Pytanie {
         this.odpowiedz = odpowiedz;
     }
 
-    public String getPodpowiedz() {
+    public int getPodpowiedz() {
         return podpowiedz;
     }
 
-    public void setPodpowiedz(String podpowiedz) {
+    public void setPodpowiedz(int podpowiedz) {
         this.podpowiedz = podpowiedz;
     }
 }
