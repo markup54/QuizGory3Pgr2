2ce042f72dfc675ed07781562ef47671879bd97a
markup54 <marzenakupka@tlen.pl>
2022-10-27 10:29:45 +0200

    przekazywanie info przez intencje

--- app/src/main/java/com/example/myapplication/PodpowiedzActivity.java
+++ app/src/main/java/com/example/myapplication/PodpowiedzActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PodpowiedzActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class PodpowiedzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podpowiedz);
-        //int aktualnyIndeks =
+        int aktualnyIndeks = getIntent().getIntExtra("Index",0);
+        Toast.makeText(PodpowiedzActivity.this,
+                String.valueOf(aktualnyIndeks),
+                Toast.LENGTH_SHORT)
+                .show();
+        ArrayList<Pytanie> pytanka = Pytanie.pytaniaArrayList;
+        TextView textViewPytanie = findViewById(R.id.podpPytaniaTextView);
+        textViewPytanie.setText(pytanka.get(aktualnyIndeks).getTresc());
+        TextView textViewPodpowiedz = findViewById(R.id.podpowiedzTextView);
+        textViewPodpowiedz.setText(pytanka.get(aktualnyIndeks).getPodpowiedz());
     }
 }
